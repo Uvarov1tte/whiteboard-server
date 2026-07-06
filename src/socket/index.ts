950332a6a963c70625618349189ca7c20c9df66e
Uvarov1tte <ngqchia@gmail.com>
2026-07-07 00:58:53 +0700

    add: support rectangle edits get updated across clients

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -6,4 +6,11 @@ export const socketActions = (socket: Socket) => {
     socket.broadcast.emit('receive-tool', tool, socket.id)
     console.log(tool)
   })
+  socket.on('add-rect', (rectangles, shapes) => {
+    // console.log(rectangles, shapes)
+    socket.broadcast.emit('receive-rect', rectangles, shapes)
+  })
+  socket.on('change-rect', (rectangles) => {
+    socket.broadcast.emit('edit-rect', rectangles)
+  })
 }
