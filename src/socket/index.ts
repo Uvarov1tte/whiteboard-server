2cb384c8cc724d08c08e2525a816f025bac06d48
Uvarov1tte <ngqchia@gmail.com>
2026-07-07 00:28:35 +0700

    add: basic socket broadcast connection to clients

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -3,6 +3,7 @@ import { Socket } from "socket.io"
 export const socketActions = (socket: Socket) => {
   console.log("user connected", socket.id)
   socket.on('change-tool', (tool: string) => {
+    socket.broadcast.emit('receive-tool', tool, socket.id)
     console.log(tool)
   })
 }
