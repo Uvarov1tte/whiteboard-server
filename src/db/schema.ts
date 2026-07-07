f66c2d9b9ef1ef46172c689dd8c5adef1e62ed37
Uvarov1tte <ngqchia@gmail.com>
2026-07-07 20:01:18 +0700

    add: boards in db

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
-import { pgTable, serial, text } from "drizzle-orm/pg-core"
+import { relations } from "drizzle-orm"
+import { pgTable, serial, text, integer, timestamp } from "drizzle-orm/pg-core"
 
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
@@ -7,5 +8,24 @@ export const users = pgTable("users", {
   passwordHash: text("password_hash").notNull().default(""),
   token: text("token")
 })
+
+export const boards = pgTable("boards", {
+  id: serial("id").primaryKey(),
+  title: text("title").notNull().default("Untitled board"),
+  userId: integer("user_id").notNull().references(() => users.id),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+})
+
+export const usersRelations = relations(users, ({ many }) => ({
+  boards: many(boards)
+}))
+
+export const boardsRelations = relations(boards, ({ one }) => ({
+  user: one(users, {
+    fields: [boards.userId],
+    references: [users.id],
+  }),
+}))
 
 // export const usersRelations = relations(users, ({ many }) => ({}))
