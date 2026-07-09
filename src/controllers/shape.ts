a1fdbcc1fec90bb4d4b3dc6873971221f97c92db
Uvarov1tte <ngqchia@gmail.com>
2026-07-09 18:42:27 +0700

    add: edited shapes got updated in db

--- src/controllers/shape.ts
+++ src/controllers/shape.ts
@@ -2,18 +2,27 @@ import { db } from '@/db/index.js';
 import { boards, shape_lists, shapes } from '@/db/schema.js';
 import { RequestCustom } from '@/types/index.js';
 import { tokenExtractor } from '@/utils/middleware.js';
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 import type { Request, Response } from 'express';
 import express from "express";
 const shapeRouter = express.Router()
 
 shapeRouter.post('/', tokenExtractor, async (req: RequestCustom, res: Response) => {
-  // const user = req.user
   const newShape = await db.insert(shapes).values(req.body).returning()
-  // console.log(newShape)
   await db.insert(shape_lists).values({ shapeId: newShape[0].id, boardId: 1 })
-  
-  res.status(201).send( newShape[0] )
+
+  res.status(201).send(newShape[0])
+})
+
+shapeRouter.put('/:id', tokenExtractor, async (req: RequestCustom, res: Response) => {
+  const editedShape = await db.update(shapes)
+    .set({
+      updatedAt: sql`NOW()`,
+      data: req.body.data
+    })
+    .where(eq(shapes.id, req.body.id))
+    .returning()
+  res.status(200).send(editedShape[0])
 })
 
 export default shapeRouter
