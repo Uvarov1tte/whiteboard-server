3f2aeca6c275007787a18cc003eaf5239c40f8c8
Uvarov1tte <ngqchia@gmail.com>
2026-07-13 13:28:25 +0700

    add: room separation per board

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,18 +2,22 @@ import { Socket } from "socket.io"
 
 export const socketActions = (socket: Socket) => {
   console.log("user connected", socket.id)
-  socket.on('change-tool', (tool: string) => {
-    socket.broadcast.emit('receive-tool', tool, socket.id)
+  socket.on("join-room", (room: string, cb: (msg: string) => void) => {
+    socket.join(room)
+    cb(`Joined ${room}`)
+  })
+  socket.on('change-tool', (tool: string, room) => {
+    socket.to(room).emit('receive-tool', tool, socket.id)
     console.log(tool)
   })
-  socket.on('add-shape', async (shape, allShapes) => {
+  socket.on('add-shape', async (shape, allShapes, room) => {
     // console.log(boardId, shape, allShapes)
     const addedShapeArr = allShapes.concat([shape])
-    console.log(addedShapeArr)
-    socket.broadcast.emit('receive-shape', shape, addedShapeArr)
+    console.log(room)
+    socket.to(room).emit('receive-shape', shape, addedShapeArr)
   })
-  socket.on('edit-shape', (shape) => {
-    console.log(shape)
-    socket.broadcast.emit('receive-edited-shape', shape)
+  socket.on('edit-shape', (shape, room) => {
+    console.log(room)
+    socket.to(room).emit('receive-edited-shape', shape)
   })
 }
