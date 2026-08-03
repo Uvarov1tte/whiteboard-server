f151a80f223041648bc1042c5683f52ba75f469d
Uvarov1tte <ngqchia@gmail.com>
2026-08-03 17:13:27 +0700

    add: route to delete shapes

--- src/routes/shape.ts
+++ src/routes/shape.ts
@@ -1,5 +1,5 @@
 import { db } from '@/db/index.js';
-import { shapes } from '@/db/schema.js';
+import { shape_lists, shapes } from '@/db/schema.js';
 import { RequestCustom } from '@/types/index.js';
 import { tokenExtractor } from '@/utils/middleware.js';
 import { eq, sql } from 'drizzle-orm';
@@ -16,6 +16,13 @@ shapeRouter.put('/:id', tokenExtractor, async (req: RequestCustom, res: Response
     .where(eq(shapes.id, req.body.id))
     .returning()
   res.status(200).send(editedShape[0])
+})
+
+shapeRouter.delete('/:id', tokenExtractor, async (req: RequestCustom, res: Response) => {
+  const id = Number(req.params.id)
+  await db.delete(shape_lists).where(eq(shape_lists.shapeId, id));
+  await db.delete(shapes).where(eq(shapes.id, id));
+  res.status(204).send({ msg: 'deleted' })
 })
 
 export default shapeRouter
